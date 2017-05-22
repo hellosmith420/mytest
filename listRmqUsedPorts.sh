8b0be650535b7343554abc38528584b3367ff368
John Smith <hellosmith420@gmail.com>
2017-05-22 10:17:55 -0400

    Update listRmqUsedPorts.sh

--- listRmqUsedPorts.sh
+++ listRmqUsedPorts.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+# search all allocated ports by rabbitmq nodes.
+#tested in rhel6 only.
 find /path/to/nodes/home -type f \( -name 'rabbitmq.config' -o -name 'rabbitmq-env.conf' \) | while read line;do grep -o '{tcp_listeners.*\|{ssl_listeners.*\|{listener,.*\|RABBITMQ_DIST_PORT.*' $line;done
